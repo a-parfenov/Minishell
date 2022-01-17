7d185e142a4ee0dec9c3d556bc1a7ebce600a885
Parfenov Anton <v5pishka@yandex.ru>
2022-01-17 22:28:56 +0300

    add exit and export + edits to other functions

--- logic/env.c
+++ logic/env.c
@@ -6,7 +6,7 @@
 /*   By: aleslie <aleslie@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/13 20:31:12 by aleslie           #+#    #+#             */
-/*   Updated: 2022/01/14 22:03:07 by aleslie          ###   ########.fr       */
+/*   Updated: 2022/01/17 14:29:33 by aleslie          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,23 @@
 
 void	command_env(t_obj	*o)
 {
-	int	i;
+	// int	i;
 
-	i = -1;
-	// write(1, "***\n", 4);
-	while (o->env[++i])
-		ft_putendl_fd(o->env[i], 1);
+	// i = -1;
+	// write(1, "\n", 1);
+	// write(1, "\n", 1);
+	// while (o->env[++i])
+	// 	ft_putendl_fd(o->env[i], 1);
+	// write(1, "\n", 1);
+	// write(1, "\n", 1);
+	t_env	*tmp;
+
+	tmp = o->env_st;
+	while (tmp != NULL)
+	{
+		ft_putendl_fd(tmp->env_str, 1);
+		tmp = tmp->next;
+	}
+	// write(1, "\n", 1);
+	// write(1, "\n", 1);
 }
--- logic/pwd.c
+++ logic/pwd.c
@@ -6,7 +6,7 @@
 /*   By: aleslie <aleslie@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/13 23:03:56 by aleslie           #+#    #+#             */
-/*   Updated: 2022/01/15 13:39:46 by aleslie          ###   ########.fr       */
+/*   Updated: 2022/01/17 17:00:55 by aleslie          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,46 @@
 
 void	command_pwd(t_obj *o)
 {
-	int	i;
+	// int	i;
 
-	i = -1;
-	while (o->env[++i])
+	// i = -1;
+	// while (o->env[++i])
+	// {
+	// 	if (ft_strncmp(o->env[i], "PWD=", 4) == 0)
+	// 	{
+	// 		ft_putstr_fd(o->env[i] + 4, 1);
+	// 		ft_putstr_fd("\n", 1);
+	// 		return ;
+	// 	}
+	// }
+	// ft_putendl_fd(o->pwd, 1);
+
+	while (o->env_st != NULL)
 	{
-		if (ft_strncmp(o->env[i], "PWD=", 4) == 0)
+		if (ft_strncmp(o->env_st->env_str, "PWD=", 4) == 0)
 		{
-			ft_putstr_fd(o->env[i] + 4, 1);
-			ft_putstr_fd("\n", 1);
-			return ;
+			ft_putendl_fd(o->env_st->env_str + 4, 1);
+			break;
 		}
+		o->env_st = o->env_st->next;
 	}
+}
+
+char	*get_address(void)
+{
+	char	*buf;
+	char	*copy;
+
+	buf = malloc(sizeof(char) * 1000);
+	copy = buf;
+	if (!buf)
+		print_error("malloc, get address", "cd");
+	buf = getcwd(buf, 1000);
+	if (!buf)
+	{
+		free(copy);
+		
+		return (NULL);
+	}
+	return (buf);
 }
